592e0bcaff28c9c40a4f5421d1525d8d349ddbaa
Rob Watson <rob@mixlr.com>
2015-08-19 10:00:01 +0100

    Add basic application.

---
+++ public/js/application.js
@@ -0,0 +1,38 @@
+// http://stackoverflow.com/questions/19491336/get-url-parameter-jquery
+var getUrlParameter = function getUrlParameter(sParam) {
+  var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+      sURLVariables = sPageURL.split('&'),
+      sParameterName,
+      i;
+
+  for (i = 0; i < sURLVariables.length; i++) {
+    sParameterName = sURLVariables[i].split('=');
+
+    if (sParameterName[0] === sParam) {
+      return sParameterName[1] === undefined ? true : sParameterName[1];
+    }
+  }
+};
+
+$(document).ready(function() {
+  soundManager.setup({
+    url: 'swf/',
+    flashVersion: 9,
+    preferFlash: true,
+    onready: function() {
+      var url = getUrlParameter('url');
+      var streamName = getUrlParameter('stream_name');
+      if(url && streamName) {
+        soundManager.createSound({
+          id: 'url',
+          serverURL: url,
+          url: streamName,
+          autoLoad: true,
+          autoPlay: true,
+          volume: 100
+        });
+      }
+    }
+  });
+});
+
